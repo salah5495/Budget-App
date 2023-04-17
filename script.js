e83d3b3735a28ecb909690907da3da28090b69ce
swaleh <73616152+salah5495@users.noreply.github.com>
2023-04-17 21:34:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict"
 
-var totalAmount = document.getElementById("total-amount");
+let totalAmount = document.getElementById("total-amount");
 let userAmount = document.getElementById("user-amount");
 const checkAmountButton = document.getElementById("check-amount");
 const totalAmountButton = document.getElementById("total-amount-button");
